6e426e7353fa2291cbbfdfa36e93d18833545f1b
pedro-batista <peddrobatista2022@gmail.com>
2024-02-22 11:43:32 -0300

    Primeiro commit...

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+	
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.sort(Program::compareProducts);
+		
+		list.forEach(System.out::println);
+	}
+
+}
